33b6c221a896cc9e2ac7068ec1cd272258bfd07f
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2020-10-19 12:11:27 +0200

    Composant App

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <!-- NAV -->
+    <Nav />
+
+    <!-- BODY -->
+    <div class="container">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+  import Nav from './components/Nav.vue'
+  import axios from 'axios'
+
+  export default {
+    name: 'App',
+    components: {
+      Nav
+    },
+    async created() {
+        const response = await axios.get('api/user');
+
+        // console.log(response);
+
+        // this.user = response.data;
+        this.$store.dispatch('user', response.data);
+    }
+}
+</script>
+
+<style>
+
+</style>
